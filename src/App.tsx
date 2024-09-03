c3dde3683bc8131ab6c9d36aa9788d4e3ec85213
odik91 <ali.shoddiqien@gmail.com>
2024-09-03 18:34:30 +0700

    git init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Dashboard, Error, Login } from "./pages"
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard></Dashboard>
+      <Login />
+      <Error />
+    </div>
+  )
+}
+export default App
